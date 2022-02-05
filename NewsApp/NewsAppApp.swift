4abc2faad784fd5f2dc007696789c88b69a40a12
OrangeFlavoredDerek <derekchan0407@gmail.com>
2022-02-05 11:02:59 +0800

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by 陳峻琦 on 5/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
